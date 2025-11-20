f59172f1c03cc40c9b362a6d4d6bd75517719662
GoodnessFx <goodnessiyamah1@gmail.com>
2025-11-20 16:36:48 +0100

    Add root route to confirm API is working

--- server/server.js
+++ server/server.js
@@ -13,11 +13,17 @@ const port = process.env.PORT || 5002;
 
 connectDB();
 
+// 👉 Add this AFTER creating app
+app.get("/", (req, res) => {
+  res.send("API is working!");
+});
+
 // Middleware
 app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5000'] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.use('/api', apiRoutes);
 
 app.listen(port, () => {
