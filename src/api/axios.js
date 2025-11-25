0c4859aa9ef1613469fdb0ff62f05f31733783c7
GoodnessFx <goodnessiyamah1@gmail.com>
2025-11-25 11:58:48 +0100

    Update frontend: describe changes here

--- src/api/axios.js
+++ src/api/axios.js
@@ -13,10 +13,11 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: '/api', // Use relative path for proxy
+  baseURL: 'https://restaurant-production-f0e2.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export default apiClient;
+
